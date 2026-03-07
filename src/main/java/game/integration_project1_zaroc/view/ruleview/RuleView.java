2fa8b2d8557a9c2c9a3ae69e0d8f85aa4ce31aa6
IlliaR2007 <illia.ramael@student.kdg.be>
2026-03-07 18:39:06 +0100

    [RuleView] Rules in ruleview with better layout. Removed excessive labels

--- src/main/java/game/integration_project1_zaroc/view/ruleview/RuleView.java
+++ src/main/java/game/integration_project1_zaroc/view/ruleview/RuleView.java
@@ -3,41 +3,27 @@ package game.integration_project1_zaroc.view.ruleview;
 
 import game.integration_project1_zaroc.view.components.GeneralActionsComponent;
 import game.integration_project1_zaroc.view.core.ResourceManager;
+
 import game.integration_project1_zaroc.view.core.fonts.Fonts;
 import game.integration_project1_zaroc.view.core.themes.Components;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
+
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RuleView extends BorderPane {
 
-    private Label titel;
 
-    private Label ruleLable1;
-    private Label ruleLable2;
-    private Label ruleLable3;
-    private Label ruleLable4;
-    private Label ruleLable5;
-    private Label ruleLable6;
-    private Label ruleLable7;
-    private Label ruleLable8;
-    private Label ruleLable9;
-    private Label ruleLable10;
-    private Label ruleLable11;
-    private Label ruleLable12;
-    private Label ruleLable13;
-    private Label ruleLable14;
-    private Label ruleLable15;
-    private Label ruleLable16;
-    private Label ruleLable17;
-    private Label ruleLable18;
-
-
+    private BorderPane ruleBorderPane;
     private Button returnButton;
     private ResourceManager resourceManager;
 
@@ -48,92 +34,77 @@ public class RuleView extends BorderPane {
     }
 
     public void initialiseNodes() {
-        titel = new Label();
+        ruleBorderPane = new BorderPane();
         this.returnButton = new GeneralActionsComponent(this.resourceManager, Components.UNDO); //TODO: make a return button and add here
-        ruleLable1 =new Label();
-        ruleLable2 = new Label();
-        ruleLable3 =new Label();
-        ruleLable4 =new Label();
-        ruleLable5 =new Label();
-        ruleLable6 =new Label();
-        ruleLable7 =new Label();
-        ruleLable8 =new Label();
-        ruleLable9 =new Label();
-        ruleLable10 =new Label();
-        ruleLable11 =new Label();
-        ruleLable12 =new Label();
-        ruleLable13 =new Label();
-        ruleLable14 =new Label();
-        ruleLable15 =new Label();
-        ruleLable16 =new Label();
-        ruleLable17 =new Label();
-        ruleLable18 =new Label();
     }
 
     public void layoutNodes() {
-        titel.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-        titel.setText("Rules: ");                      //zou een title lettertypen moeten hebben
-        VBox rules = new VBox(titel,ruleLable1,ruleLable2,ruleLable3,ruleLable4,ruleLable5,ruleLable6,ruleLable7,ruleLable8,ruleLable9,ruleLable10,ruleLable11,ruleLable12,ruleLable13,ruleLable14,ruleLable15,ruleLable16,ruleLable17,ruleLable18);
-        rules.setSpacing(10);
-
-        ruleLable1.setText("Spelers");
-        ruleLable1.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable2.setText("Zaroc is een abstract strategiespel voor twee spelers. Elke speler speelt met stukken van één kleur.");
-        ruleLable2.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable3.setText("Doel van het spel");
-        ruleLable3.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable4.setText("Het doel van het spel is om als eerste drie van je eigen stukken in de eindrij te plaatsen. De eindrij bestaat uit de vijf vakjes aan het einde\nvan het bord.");
-        ruleLable4.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable5.setText("Opstelling");
-        ruleLable5.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable6.setText("Het bord bestaat uit 13 pinnen (palen) met afnemende hoogte, die naar de eindrij leiden. Aan het begin van het spel worden de vier hoogste\npinnen gevuld met telkens vier stukken, geplaatst in afwisselende kleuren. Elke pin vormt een stapel,\nen alleen het bovenste stuk van een stapel mag verplaatst worden.");
-        ruleLable6.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable7.setText("Beurtverloop");
-        ruleLable7.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable8.setText("Spelers spelen om de beurt. Tijdens je beurt moet je twee zetten uitvoeren. Deze twee zetten mogen met eender welk stuk op het bord gebeuren,\nook met stukken van je tegenstander. Je mag ook twee keer hetzelfde stuk verplaatsen, zolang de zetten geldig zijn.");
-        ruleLable8.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable9.setText("Beweging");
-        ruleLable9.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable10.setText("Bij elke zet neem je het bovenste stuk van een pin en verplaats je het zijwaarts of vooruit.");
-        ruleLable10.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable11.setText("Zijwaartse beweging");
-        ruleLable11.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));             //zou een tussenin lettertype moeten hebben
-
-        ruleLable12.setText("Bij een zijwaartse zet verplaats je een stuk naar een aangrenzende pin van dezelfde hoogte, op voorwaarde dat er nog plaats is op die pin.");
-        ruleLable12.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable13.setText("Voorwaartse beweging");                                       // tussenin lettertype
-        ruleLable13.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable14.setText("Bij een voorwaartse zet verplaats je een stuk naar een pin van een lagere hoogte of naar een vakje in de eindrij, volgens de voorwaartse\nverbindingen van het bord. Een stuk mag alleen vooruit bewegen wanneer het zich op de hoogst mogelijke positie van zijn pin bevindt,\ndus wanneer er geen lege plaatsen boven het stuk zijn.");
-        ruleLable14.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable15.setText("Beperkingen");
-        ruleLable15.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable16.setText("Stukken mogen nooit achteruit bewegen naar een hogere pin. Een zet mag de vorige zet niet onmiddellijk ongedaan maken. Dat betekent dat het\nlaatst verplaatste stuk niet meteen terug naar zijn vorige plaats mag worden verplaatst, zelfs niet door de andere speler.\nStukken die de eindrij bereiken mogen niet meer bewegen, en zijwaartse beweging is in de eindrij niet toegestaan.");
-        ruleLable16.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
-
-        ruleLable17.setText("Het spel winnen");
-        ruleLable17.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
-
-        ruleLable18.setText("Het spel eindigt onmiddellijk wanneer een speler drie stukken van zijn kleur in de eindrij heeft. Die speler wint het spel.");
-        ruleLable18.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
 
 
-        setCenter(rules);
-        BorderPane.setAlignment(rules, Pos.CENTER);
-        BorderPane.setMargin(rules, new Insets(150,0,0,200));
+        //zou een title lettertypen moeten hebben
+        Text titel = new Text("Regels:\n\n");
+        Text hText1 = new Text("Doel van het spel\n\n");
+        Text sText1 = new Text("Het doel van het spel is om als eerste drie van je eigen stukken in de eindrij te plaatsen. De eindrij bestaat uit de vijf vakjes aan het einde van het bord.\n\n");
+        Text hText2 = new Text("Beurtverloop\n\n");
+        Text sText2 = new Text("Spelers spelen om de beurt. Tijdens je beurt moet je twee zetten uitvoeren. Deze twee zetten mogen met eender welk stuk op het bord gebeuren, ook met stukken van je tegenstander. Je mag ook twee keer hetzelfde stuk verplaatsen, zolang de zetten geldig zijn.\n\n");
+        Text hText3 = new Text("Beweging\n\n");
+        Text sText3 = new Text("""
+                Bij elke zet neem je het bovenste stuk van een pin en verplaats je het zijwaarts of vooruit.
+                Bij een zijwaartse zet verplaats je een stuk naar een aangrenzende pin van dezelfde hoogte, op voorwaarde dat er nog plaats is op die pin.
+                Bij een voorwaartse zet verplaats je een stuk naar een pin van een lagere hoogte of naar een vakje in de eindrij, volgens de voorwaartse verbindingen van het bord. Een stuk mag alleen vooruit bewegen wanneer het zich op de hoogst mogelijke positie van zijn pin bevindt, dus wanneer er geen lege plaatsen boven het stuk zijn.
+                Stukken mogen nooit achteruit bewegen naar een hogere pin.
+                Een zet mag de vorige zet niet onmiddellijk ongedaan maken. Dat betekent dat het laatst verplaatste stuk niet meteen terug naar zijn vorige plaats mag worden verplaatst, zelfs niet door de andere speler.
+                Stukken die de eindrij bereiken mogen niet meer bewegen, en zijwaartse beweging is in de eindrij niet toegestaan.
+                
+                """);
+        Text hText4 = new Text("Het spel winnen\n\n");
+        Text sText4 = new Text("Het spel eindigt onmiddellijk wanneer een speler drie stukken van zijn kleur in de eindrij heeft. Die speler wint het spel.");
+
+        List<Text> small = new ArrayList<>();
+        small.add(sText1);
+        small.add(sText2);
+        small.add(sText3);
+        small.add(sText4);
+
+        List<Text> big = new ArrayList<>();
+        big.add(titel);
+        big.add(hText1);
+        big.add(hText2);
+        big.add(hText3);
+        big.add(hText4);
+
+        for (Text text : small) {
+            text.setFont(resourceManager.getFont(Fonts.PRESSSTART2PSMALL));
+        }
+
+        for (Text text : big) {
+            text.setFont(resourceManager.getFont(Fonts.PRESSSTART2PLARGE));
+        }
+
+
+        TextFlow rules = new TextFlow(titel, hText1, sText1, hText2, sText2, hText3, sText3, hText4, sText4);
+        rules.setLineSpacing(5);
+
+
+        Image boardBackgroundImage = resourceManager.getImage(Components.PEGVIEW);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(
+                boardBackgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                backgroundSize
+        );
+
+        ruleBorderPane.setBackground(new Background(backgroundImage));
+        ruleBorderPane.setMaxSize(1000, 800);
+        ruleBorderPane.setPrefSize(1000, 800);
+        ruleBorderPane.setCenter(rules);
+        ruleBorderPane.setPadding(new Insets(50,150,50,150));
+
+        setCenter(ruleBorderPane);
+        BorderPane.setAlignment(ruleBorderPane, Pos.CENTER);
+        BorderPane.setMargin(ruleBorderPane, new Insets(50,170 ,180 ,170 )); //top , right, bottom, left
 
         setTop(returnButton);
         BorderPane.setAlignment(returnButton, Pos.TOP_LEFT);
