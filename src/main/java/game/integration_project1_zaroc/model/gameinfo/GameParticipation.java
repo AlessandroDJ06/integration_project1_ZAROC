01f6e9af7f0dc865921917f8273d24ea1c05e5e7
Lasha Dularidze <lasha.dularidze@student.kdg.be>
2026-03-13 10:15:29 +0100

    [model] - Game now uses GameParticipation in the constructor

--- src/main/java/game/integration_project1_zaroc/model/gameinfo/GameParticipation.java
+++ src/main/java/game/integration_project1_zaroc/model/gameinfo/GameParticipation.java
@@ -1,7 +1,5 @@
 package game.integration_project1_zaroc.model.gameinfo;
 
-import game.integration_project1_zaroc.model.players.AIPlayer;
-import game.integration_project1_zaroc.model.players.HumanPlayer;
 import game.integration_project1_zaroc.model.players.Player;
 
 
