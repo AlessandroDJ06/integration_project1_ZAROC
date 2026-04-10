6aaa6511eb4ec675d0381bdc02e21a266029dd6b
Alessandro de Jongh <alessandrodejongh@outlook.com>
2026-04-10 20:44:16 +0200

    [create accountview] added hover animations to the buttons

--- src/main/java/game/integration_project1_zaroc/view/pages/createaccountview/CreateAccountPresenter.java
+++ src/main/java/game/integration_project1_zaroc/view/pages/createaccountview/CreateAccountPresenter.java
@@ -3,8 +3,13 @@ package game.integration_project1_zaroc.view.pages.createaccountview;
 import game.integration_project1_zaroc.dao.ZarocDaoException;
 import game.integration_project1_zaroc.model.AppController;
 import game.integration_project1_zaroc.view.sharedlogic.resource_manager.profilePictures.ProfilePictures;
+import game.integration_project1_zaroc.view.sharedlogic.utils.GeneralEventhandlers;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CreateAccountPresenter {
     private AppController model;
@@ -18,6 +23,19 @@ public class CreateAccountPresenter {
     }
 
     private void addEventHandlers() {
+        List<Button> buttons = Arrays.asList(
+                view.getCreateButton(),
+                view.getReturnButton(),
+                view.getProfilePicturePicker().getLeftButton(),
+                view.getProfilePicturePicker().getRightButton()
+        );
+
+        for (Button button : buttons){
+            GeneralEventhandlers.addHoverEffect(button);
+        }
+
+
+
         view.getReturnButton().setOnAction(e -> closeWindow());
 
         view.getCreateButton().setOnAction(e -> {
