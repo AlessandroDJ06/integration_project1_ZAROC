0582fc83681112e1e613081ed0e7b12e7faf72f4
IlliaR2007 <illia.ramael@student.kdg.be>
2026-04-11 15:50:58 +0200

    [Settingsview/presenter] added icons and eventhandlers so icons change depending on if the sound/music is on or off

--- src/main/java/game/integration_project1_zaroc/view/pages/settingsview/SettingsPresenter.java
+++ src/main/java/game/integration_project1_zaroc/view/pages/settingsview/SettingsPresenter.java
@@ -3,6 +3,9 @@ package game.integration_project1_zaroc.view.pages.settingsview;
 import game.integration_project1_zaroc.model.AppController;
 import game.integration_project1_zaroc.view.pages.ruleview.RuleView;
 import game.integration_project1_zaroc.view.sharedlogic.utils.GeneralEventhandlers;
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class SettingsPresenter {
@@ -23,6 +26,44 @@ public class SettingsPresenter {
                 stage.close();
             });
             GeneralEventhandlers.addHoverEffect(view.getReturnButton());
-        }
 
+            view.getVolumeSlider().setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    setVolumeIcon();
+                }
+            });
+
+            view.getVolumeSlider().setOnMouseClicked(event -> {
+               setVolumeIcon();
+            });
+            view.getSoundSlider().setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    setSoundIcon();
+                }
+            });
+            view.getSoundSlider().setOnMouseClicked(event -> {
+                setSoundIcon();
+            });
+
+
+
+        }
+    private void setVolumeIcon() {
+        double currentValue = view.getVolumeSlider().getValue();
+        if(currentValue==0){
+            view.setVolumeIcon(new Image(getClass().getResource("/game/integration_project1_zaroc/ui/icons/mute_music.png").toExternalForm()));
+        }else{
+            view.setVolumeIcon(new Image(getClass().getResource("/game/integration_project1_zaroc/ui/icons/music.png").toExternalForm()));
+        }
+    }
+    private void setSoundIcon() {
+        double currentValue = view.getSoundSlider().getValue();
+        if(currentValue==0){
+            view.setSoundIcon(new Image(getClass().getResource("/game/integration_project1_zaroc/ui/icons/mute_sound.png").toExternalForm()));
+        }else{
+            view.setSoundIcon(new Image(getClass().getResource("/game/integration_project1_zaroc/ui/icons/sound.png").toExternalForm()));
+        }
+    }
 }
